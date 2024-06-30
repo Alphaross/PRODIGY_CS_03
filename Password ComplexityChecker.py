a445986a726f310dfebd0f8c506256310fcf1682
Roshan Manke <95066133+Alphaross@users.noreply.github.com>
2024-06-30 17:30:34 +0530

    Add files via upload

---
+++ Password ComplexityChecker.py
@@ -0,0 +1,34 @@
+def password(user_input):
+
+	n = len(user_input)
+
+	hasLower = False
+	hasUpper = False
+	hasDigit = False
+	specialChars = False
+	normalChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 "
+
+	for i in range(n):
+		if user_input[i].islower():
+			hasLower = True
+		if user_input[i].isupper():
+			hasUpper = True
+		if user_input[i].isdigit():
+			hasDigit = True
+		if user_input[i] not in normalChars:
+			specialChars = True
+
+
+	print("Strength of password:-", end = "")
+	if (hasLower and hasUpper and hasDigit and specialChars and n >= 8):
+		print("Very Strong")
+
+	elif ((hasLower or hasUpper) and specialChars and n >= 6):
+		print("Moderate")
+	else:
+		print("Weak")
+
+
+if __name__=="__main__":
+    user_input = input("Enter the password :-")
+    password(user_input)
